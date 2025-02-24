c24b5edc03d4fb685c7d981c1f0136f087ae6091
Henrique Lopes <h.lopes.silva2015@gmail.com>
2025-02-23 21:51:04 -0300

    Change: Styles Button

--- src/components/atoms/Button/styled.ts
+++ src/components/atoms/Button/styled.ts
@@ -2,4 +2,14 @@ import styled from "styled-components";
 
 export const Button = styled.button`
   background-color: ${({ theme }) => theme.colors.cyan[400]};
+  padding: 0.5rem 1rem;
+  border: 1px solid ${({ theme }) => theme.colors.cyan[500]};
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: all ${({ theme }) => theme.animation.duration.xxSlow};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.cyan[500]};
+    border: 1px solid ${({ theme }) => theme.colors.cyan[600]};
+  }
 `;
