ba23188018beda0bff8ebb9516ca1a266d94c5ed
Henrique Lopes <h.lopes.silva2015@gmail.com>
2025-02-23 21:52:22 -0300

    Test: Husky

--- src/components/pages/Homepage/index.tsx
+++ src/components/pages/Homepage/index.tsx
@@ -1,38 +1,22 @@
 import { useState } from "react";
 
-import reactLogo from "@assets/images/react.svg";
-import viteLogo from "/logo.svg";
 import { Button } from "@atoms/Button";
-import { Modal } from "@mui/material";
+
+import S from "./styled";
+import Modal from "@molecules/Modal";
 
 export function Homepage() {
-  const [count, setCount] = useState(0);
+  const [modal, setModal] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <S.Container>
       <h1>Vite + React</h1>
-      <div className="card">
-        <Modal open={Boolean(count)} onClose={() => setCount(0)}>
-          <div>teste</div>
-        </Modal>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <Button onClick={() => setModal(true)}>Abrir Modal</Button
+
+      <Modal open={modal} onClose={() => setModal(false)}>
+        <div>teste</div>
+      </Modal>
+    </S.Container>
   );
 }
